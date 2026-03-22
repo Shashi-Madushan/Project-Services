145f09056e0ef545d342849ee537bf7b9b731fbc
Shashi Madushan <156106584+Shashi-Madushan@users.noreply.github.com>
2026-03-22 12:01:00 +0530

    add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    apps: [
+//        {
+//          name: "cloud-sql-auth-proxy",
+//          script: "./cloud-sql-proxy its-2130-eca-hdse-69-70-489518:asia-southeast1:mysql-vm its-2130-eca-hdse-69-70-489518:asia-southeast1:postgres-vm --private-ip",
+//          log_file: "./logs/cloud-sql-proxy.log",
+//        },
+        {
+            name: "iam-service",
+            script: "java -jar iam-service/target/iam-service-1.0.0.jar",
+            log_file: "./logs/customer-management-service.log",
+            instances: 2,
+
+        },
+        {
+            name: "product-service",
+            script: "java -jar product-service/target/product-service-1.0.0.jar",
+            log_file: "./logs/product-catalog-service.log",
+            instances: 2,
+
+        },
+        {
+            name: "order-service",
+            script: "java -jar order-service/target/Order-Service-1.0.0.jar",
+            log_file: "./logs/order-service.log",
+            instances: 2,
+
+        }
+    ]
+}
